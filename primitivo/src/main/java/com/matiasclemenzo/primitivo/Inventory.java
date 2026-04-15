35366bb8b9588f7f1efdf5709d7011922b4a577c
Matías Clemenzo <cmzo@protonmail.com>
2026-04-15 16:36:18 +0200

    Se completa la clase Inventory

--- primitivo/src/main/java/com/matiasclemenzo/primitivo/Inventory.java
+++ primitivo/src/main/java/com/matiasclemenzo/primitivo/Inventory.java
@@ -1,5 +1,59 @@
 package com.matiasclemenzo.primitivo;
 
+import java.util.List;
+
+/**
+ * Muestra la mochila del personaje
+ * 
+ * 
+ */
 public class Inventory {
+    private List<Item> items;
+    private Weapon equippedWeapon;
+    private Armor equippedArmor;
     
+    public Inventory(List<Item> items) {
+        this.items = items;
+
+    }
+
+    /**
+    * Añade un item al listado de items
+    * @param item es el objeto que deseo añadir a la lista
+    * 
+    */    
+    public void addItem(Item item) {
+        items.add(item);
+    }
+
+    /**
+    * Quita un item del listado de items
+    * @param item es el objeto que deseo quitar de la lista
+    * 
+    */
+    public void removeItem(Item item) {
+        items.remove(item);
+    }
+
+    /**
+    * Verifica si el item es un arma o una armadura y la equipa según corresponda
+    * @param item es el objeto que deseo equipar
+    * 
+    */
+    public void equipItem(Item item) {
+        if (item instanceof Weapon) {
+            equippedWeapon = (Weapon) item;
+        } else if (item instanceof Armor) {
+            equippedArmor = (Armor) item;
+        } else {
+            System.out.println("Este item no se puede equipar");
+        }
 }
+
+    public void listItems() {
+        for (Item item : items) {
+            System.out.println(item.getName());
+        }
+}
+}
+
